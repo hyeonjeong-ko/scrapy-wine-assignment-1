abeb3e1c813e099a28762db242b73dc94673d317
Kohyeonjeong <toyu7870@naver.com>
2023-11-19 16:11:31 +0900

    update wineitem 아이템

---
+++ items.py
@@ -0,0 +1,24 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WineItem(scrapy.Item):
+    _id = scrapy.Field()
+    wine_image = scrapy.Field()
+    winery_name = scrapy.Field()
+    wine_name = scrapy.Field()
+    country = scrapy.Field()
+    region = scrapy.Field()
+    rating = scrapy.Field()
+    ratings_count = scrapy.Field()
+    link = scrapy.Field()
+    price = scrapy.Field()
+
+    average_price = scrapy.Field()  # 평균 가격
+    taste_like = scrapy.Field()  # 맛 구조 정보
+    keywords = scrapy.Field()  # 키워드
+    reviews = scrapy.Field()  # 리뷰
